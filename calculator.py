bee1714a4e6c4b6979b939cfde15cdd4088c7858
Polus Gorgees <polusgorgees123@gmail.com>
2021-10-13 00:03:15 -0700

    fifth commit

--- calculator.py
+++ calculator.py
@@ -4,11 +4,11 @@ def calculator(number1,number2,operator):
 		print(number1 + number2)
 	elif operator == "-":
 		print(number1 - number2)
-	if operator == "*":
+	elif operator == "*":
         	print(number1 * number2)
         elif operator == "/":
         	print(number1 / number2)
-	if operator == "//":
+	elif operator == "//":
         	print(number1 // number2)
         elif operator == "**":
         	print(number1 ** number2)
@@ -16,8 +16,9 @@ def calculator(number1,number2,operator):
 		return flase
 def parse_input():
 	while True:
-		n1 = flaot(input("Enter the first number:"))
-		n2 = flaot(input("Enter the second number:"))
-		op = input("Enter the operator:")
+		n1 = flaot(input("Enter the first number: "))
+		n2 = flaot(input("Enter the second number: "))
+		op = input("Enter the operator: ")
 		if not (op=="+" or op=="-" or op=="*" or op=="/" or op=="//" or op=="**"):
 			break
+		calculator(n1,n2,op)
